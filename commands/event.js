48aa0d3d8ff243f08e82092abbdfe9d0b0940d92
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-06-08 05:34:14 +0200

    ⚡️ Added Timestamp for the /event Command for better Reading the Time!

--- commands/event.js
+++ commands/event.js
@@ -20,13 +20,17 @@ module.exports = {
          if (!activeEvent) {
             await interaction.reply({ content: 'There is no active event currently.', ephemeral: true });
          } else {
+            const eventStartTime = Math.floor(activeEvent.createdAt.getTime() / 1000); // Event start time in Unix timestamp
+            const eventEndTime = eventStartTime + (activeEvent.duration * 3600); // Event end time in Unix timestamp
+
             const embed = new EmbedBuilder()
                .setTitle(`Active Event: ${activeEvent.name}`)
                .setDescription(activeEvent.desc)
                .addFields(
                   { name: 'Multiplier', value: activeEvent.multiplier.toString(), inline: true },
                   { name: 'Duration', value: `${activeEvent.duration} hours`, inline: true },
-                  { name: 'Started At', value: activeEvent.createdAt.toLocaleString(), inline: true }
+                  { name: 'Started At', value: `<t:${eventStartTime}:R>`, inline: true },
+                  { name: 'Remaining Time', value: `<t:${eventEndTime}:R>`, inline: true }
                )
                .setColor('Random')
                .setTimestamp()
