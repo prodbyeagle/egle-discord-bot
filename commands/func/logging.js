4cc822316d6d05576664c2208838756b6710d72e
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-06-13 17:15:31 +0200

    ⚡️ Added Command Logging

---
+++ commands/func/logging.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require('discord.js');
+
+async function logCommand(client, command, user) {
+   const logChannelId = '1250827063158378518'; // ID des Log-Channels
+   const logChannel = await client.channels.fetch(logChannelId);
+
+   if (!logChannel) {
+      console.error('Log channel not found');
+      return;
+   }
+
+   const timestamp = Math.floor(Date.now() / 1000);
+
+   const embed = new EmbedBuilder()
+      .setTitle('Command Log')
+      .setColor('Yellow')
+      .addFields(
+         { name: 'User', value: `<@${user.id}> (${user.id})`, inline: true },
+         { name: 'Command', value: command, inline: true },
+         { name: 'Timestamp', value: `<t:${timestamp}:R>`, inline: false }
+      )
+      .setFooter({ text: `🦅 made by @prodbyeagle` })
+      .setTimestamp();
+
+   await logChannel.send({ embeds: [embed] });
+}
+
+module.exports = { logCommand };
