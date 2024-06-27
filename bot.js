779833bcca7b3d9991a06cf3c9a59d554fffe4b8
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-06-27 05:00:36 +0200

    🔥 Removed /giveawayXP Command

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ for (const file of commandFiles) {
    client.commands.set(command.data.name, command);
 }
 
-client.currentMode = Modes.ONLINE;
+client.currentMode = Modes.DEBUG;
 
 client.once('ready', async () => {
    debug(`Logged in as ${client.user.tag}`, 'login');
